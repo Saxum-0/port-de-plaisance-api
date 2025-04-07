b31c84ce8f04f08911c61b99bc92b1c13b3fb3fb
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-04-07 17:56:53 +0200

    port

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ mongoose.connection.once("open", () => {
 });
 
 // Lancement du serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
 });
